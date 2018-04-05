e914ac3d5b39478cbe5b284bba68be175eb873c2
Alen Šimec <alen@eburza.hr>
2018-04-05 13:55:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
   <title>Php zadaca</title>
 </head>
 <body>
-<?php 
-    function check_time($d, $ot, $ct) {
-        return ($d >= $ot && $d <= $ct);
-    }
-
-    $d = date('H');
-    echo "Dućan je " . (check_time($d, "8", "16") ? "otvoren" : "zatvoren");
+<?php     
+  function check_time($d, $ot, $ct) { 
+       return ($d >= $ot && $d <= $ct);  
+  }   
+  $d = date('H');    
+  echo "<p>Dućan je otvoren od 8h do 16h.</p>
+  <p>Sada je $d sati!</p>
+  <p>Dućan je " . (check_time($d, "8", "16") ? "otvoren" : "zatvoren") . "</p>";
 ?>
-
 
 
 </body>
